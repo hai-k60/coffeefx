db6c68dfad28111c45bbeb8a08ccc135bff39e63
MyPC <MyPC@DESKTOP-KAITO>
2019-12-14 12:53:45 +0700

    login

--- src/coffeefx/FXMLDocumentController.java
+++ src/coffeefx/FXMLDocumentController.java
@@ -14,8 +14,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -28,25 +33,53 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
+    private PasswordField txt_password;
+    
+    @FXML
+    private TextField txt_username;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("home.fxml"));
-            /* 
-             * if "fx:controller" is not set in fxml
-             * fxmlLoader.setController(NewWindowController);
-             */
-            Scene scene = new Scene(fxmlLoader.load());
-            Stage stage = new Stage();
-            stage.setTitle("New Window");
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-        Logger logger = Logger.getLogger(getClass().getName());
-        logger.log(Level.SEVERE, "Failed to create new Window.", e);
-    }
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+        
+        //Xu ly dang nhap
+        if(username.equals("admin") && password.equals("admin")){
+            System.out.println("Dang nhap thanh cong");
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("home.fxml"));
+                /* 
+                 * if "fx:controller" is not set in fxml
+                 * fxmlLoader.setController(NewWindowController);
+                 */
+                Scene scene = new Scene(fxmlLoader.load());
+                Stage stage = new Stage();
+                stage.setTitle("New Window");
+                stage.setScene(scene);
+                stage.show();
+                ((Node)(event.getSource())).getScene().getWindow().hide();
+            } catch (IOException e) {
+                Logger logger = Logger.getLogger(getClass().getName());
+                logger.log(Level.SEVERE, "Failed to create new Window.", e);
+            }
+        } else{
+            System.out.println("Dang nhap that bai");
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Login");
+
+            // Header Text: null
+            alert.setHeaderText(null);
+            alert.setContentText("Tài khoản hoặc mật khẩu không đúng");
+
+            alert.showAndWait();
+        }
+        
+        
+        
+        
     }
     
     @Override
